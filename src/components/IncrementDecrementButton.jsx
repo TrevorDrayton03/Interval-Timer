7e2f1826a8754b8ac0a7dda577b0ab49840c84f5
trevordrayton <trevorpdrayton@gmail.com>
2022-10-08 16:57:01 -0700

    Timer component accepts name and icon as props

---
+++ src/components/IncrementDecrementButton.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
+
+const IncrementDecrementButtons = () => {
+    return (
+        0
+    )
+};
+
+const styles = StyleSheet.create({
+    timerContainer: {
+        //flex: 0,
+        //width: width,
+        height: height / 9,
+        justifyContent: "center",
+        paddingHorizontal: 10
+    },
+    button: {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10
+    },
+    row: {
+        flexDirection: "row",
+        width: width,
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        marginTop: 20,
+    },
+    column: {
+        //flexDirection: "column", //this appears to be implied
+        //flex: 3,
+        width: width / 5,
+        justifyContent: "center",
+        paddingHorizontal: 20,
+        //marginTop: 20,
+    },
+    buttonColumn: {
+        //flexDirection: "column", //this appears to be implied
+        //flex: 2,
+        //width: width,
+        justifyContent: "center",
+        //paddingHorizontal: 20,
+        //marginTop: 20,
+    },
+    buttonRow: {
+        //flexDirection: "column", //this appears to be implied
+        //flex: 2,
+        //width: width,
+        justifyContent: "flex-end",
+        //paddingHorizontal: 20,
+        //marginTop: 20,
+    },
+});
