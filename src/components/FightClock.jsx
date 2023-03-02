8be5beb2b3e8a3c047df8b282cacc5ce7ca0d3a7
trevordrayton <trevorpdrayton@gmail.com>
2023-03-02 09:23:59 -0800

    starting on the path of the solution (comparing time differences over app states)

--- src/components/FightClock.jsx
+++ src/components/FightClock.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Modal, View, Text, TouchableOpacity, Alert } from "react-native";
+import { Modal, View, Text, TouchableOpacity, Alert, AppState } from "react-native";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from "../styles/styles"
 import helpers from "../helpers/helpers"
+
+// AppState example:
+// https://snack.expo.dev/@aboutreact/appstate-example?session_id=snack-session-y4eB29bZK
 
 const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
     const [duration, setDuration] = useState((readyLength > 0 ? readyLength - 1 : roundLength - 1));
@@ -12,9 +15,28 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
     const [rest, setRest] = useState(false);
     const [ready, setReady] = useState(false);
     const [complete, setComplete] = useState(false);
+    const [appState, setAppState] = useState(AppState.currentState);
+    const [stopTime, setStopTime] = useState(null);
+
 
     let displayTime = helpers.displayTime(duration)
 
+    const handleAppStateChange = (nextAppState) => {
+        console.log('App State: ' + nextAppState);
+        if (appState === 'active' && nextAppState.match(/inactive|background/)) {
+            setStopTime(Date.now())
+            console.log('App State: ' + 'App has come to the background!');
+            console.log(Date.now());
+        }
+        setAppState(nextAppState);
+    };
+
+    useEffect(() => {
+        subscription = AppState.addEventListener('change', handleAppStateChange);
+        return () => {
+            subscription.remove()
+        };
+    }, []);
     useEffect(() => {
         if (readyLength > 0) {
             setReady(true)
@@ -27,7 +49,6 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
         setRest(false);
         setComplete(false);
     }, [modalVisible, roundLength])
-
     useEffect(() => {
         if (rest) {
             setDuration(restLength > 0 ? restLength - 1 : roundLength - 1);
@@ -35,7 +56,6 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
             setDuration(roundLength - 1);
         }
     }, [rest]);
-
     useEffect(() => {
         if (!ready) {
             setDuration(roundLength - 1);
@@ -43,17 +63,24 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
             setDuration(readyLength - 1)
         }
     }, [ready]);
-
     // manages if there is multiple rounds with no rest
     useEffect(() => {
         if (!rest && !ready) {
             setDuration(roundLength - 1);
         }
     }, [rounds]);
-
     // the key to getting the FightClock to work as intended is having this use effect trigger the above use effects [rest, ready, rounds]
     // otherwise, there is one too many steps in the interval that could not be avoided
     useEffect(() => {
+        if (stopTime !== null) { // if the timer has been stopped before
+            const elapsedTime = Math.floor((Date.now() - stopTime) / 1000); // calculate the elapsed time in seconds
+            const remainingTime = duration - elapsedTime; // calculate the remaining time
+            if (remainingTime > 0) { // if there is remaining time
+                setDuration(remainingTime); // update the duration state variable
+            } else { // if there is no remaining time
+                setDuration(roundLength - 1); // reset the duration state variable
+            }
+        }
         if (ready && duration == -1 && rounds == 1) {
             setReady(false)
         }
@@ -73,6 +100,7 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
             setTraining(null);
             setComplete(true);
         }
+        setStopTime(null); // reset the stopTime state variable
     }, [duration]);
 
     const onPressHandle = () => {
