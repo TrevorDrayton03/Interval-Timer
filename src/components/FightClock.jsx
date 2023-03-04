a9a54c30ff0e8f7c7e1c738c7fd8a073e2d49276
trevordrayton <trevorpdrayton@gmail.com>
2023-03-04 13:29:41 -0800

    appears to have no more bugs and function fully correctly

--- src/components/FightClock.jsx
+++ src/components/FightClock.jsx
@@ -18,22 +18,19 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
     const [appState, setAppState] = useState(AppState.currentState);
     const [stopTime, setStopTime] = useState(null);
     const [startTime, setStartTime] = useState(null);
-    //const [totalDuration, setTotalDuration] = useState(roundLength * intervals + restLength * (intervals - 1) + readyLength)
+    const [alteringState, setAlteringState] = useState(false);
 
     let totalDuration = roundLength * intervals + restLength * (intervals - 1) + readyLength;
     let displayTime = helpers.displayTime(duration)
 
     const handleAppStateChange = (nextAppState) => {
-        console.log("handleAppStateChange")
-        if (appState === 'active' && nextAppState.match(/inactive|background/)) {
+        if (appState === 'active' && nextAppState.match(/inactive|background/) && modalVisible) {
             setStopTime(Date.now())
         }
         setAppState(nextAppState);
     };
 
-    // instantiate an event listener for appstate changes upon component creation
     useEffect(() => {
-        console.log("appstate event listeer")
         subscription = AppState.addEventListener('change', handleAppStateChange);
         return () => {
             subscription.remove()
@@ -41,195 +38,139 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
     }, []);
 
     useEffect(() => {
-        if (readyLength > 0) {
-            setReady(true)
+        if (!alteringState) {
+            console.log("rest useEffect")
+            if (rest) {
+                setDuration(restLength > 0 ? restLength - 1 : roundLength - 1);
+            } else {
+                setDuration(roundLength - 1);
+            }
         }
-        else {
-            setReady(false)
-        }
-        setDuration(readyLength > 0 ? readyLength - 1 : roundLength - 1);
-        setRounds(1);
-        setRest(false);
-        setComplete(false);
-
-    }, [modalVisible, roundLength])
-    useEffect(() => {
-        console.log("rest useEffect")
-        if (rest) {
-            setDuration(restLength > 0 ? restLength - 1 : roundLength - 1);
-        } else {
-            setDuration(roundLength - 1);
-        }
+        setAlteringState(false)
     }, [rest]);
 
     useEffect(() => {
-        console.log("ready useEffect")
-        if (!ready) {
-            setDuration(roundLength - 1);
-        } else {
-            setDuration(readyLength - 1)
+        if (!alteringState) {
+            console.log("ready useEffect")
+            if (!ready) {
+                setDuration(roundLength - 1);
+            } else {
+                setDuration(readyLength - 1)
+            }
         }
-    }, [ready]);
+        setAlteringState(false)
+    }, [ready,]);
 
-    // manages if there is multiple rounds with no rest
     useEffect(() => {
-        console.log("rounds useEffect")
-        if (!rest && !ready) {
-            setDuration(roundLength - 1);
+        if (!alteringState) {
+            console.log("rounds useEffect")
+            if (!rest && !ready) {
+                setDuration(roundLength - 1);
+            }
         }
+        setAlteringState(false)
     }, [rounds]);
 
     // the key to getting the FightClock to work as intended is having this use effect trigger the above use effects [rest, ready, rounds]
     // otherwise, there is one too many steps in the interval that could not be avoided
     useEffect(() => {
-        console.log("duration useEffect")
         if (stopTime !== null) {
-            console.log("appState useEffect stopTime not null")
-            //const offSet = stopTime - startTime; 
-            //const elapsedTime = Date.now() - stopTime;
-            //const currentTime = Math.floor((offSet + elapsedTime) / 1000);
+            setAlteringState(true)
+            console.log("stoptime not null")
             const currentTime = Math.floor((Date.now() - startTime) / 1000);
             const remainingTime = totalDuration - currentTime;
-            //////////////////////////////////////////////// TIME REMAINING ///////////////////////////////////////////////////////////
             // if the timer is not done
             if (remainingTime > 0) {
                 // if should be in ready state
                 if (currentTime < readyLength) {
-                    //setReady(true); should already be set this way
-                    //setRest(false); should already be set this way
-                    console.log(totalDuration, " totalDuration")
-                    console.log(remainingTime, " remainingTime")
-                    console.log(readyLength, " readyLength")
-                    console.log(currentTime)
-                    console.log(readyLength - currentTime, "ready length duration")
                     setDuration(readyLength - currentTime);
                 }
                 // if should be in round or rest state
                 else {
                     setReady(false);
                     // do logic with remaining duration to determine which state the app needs to be in 
-                    const timeline = [];
-                    for (var i = 1; i <= intervals; i++) {
-                        let ti_1 = (roundLength * i + restLength * (i - 1) + readyLength)
-                        let ti_2 = (roundLength * i + restLength * i + readyLength)
-                        if (i != intervals) {
-                            timeline.push(ti_1, ti_2)
-                        }
-                        else {
-                            timeline.push(ti_1)
+                    const timeline = [readyLength];
+                    if (restLength > 0) {
+                        for (let i = 1; i <= intervals; i++) {
+                            let ti_1 = (roundLength * i + restLength * (i - 1) + readyLength)
+                            let ti_2 = (roundLength * i + restLength * i + readyLength)
+                            if (i != intervals) {
+                                timeline.push(ti_1, ti_2)
+                            }
+                            else {
+                                timeline.push(ti_1)
+                            }
                         }
                     }
-                    console.log(timeline)
-                    // loop through timeline to determine where current time stands
-                    for (var i = 0; i < timeline.length; i++) {
-                        // round
-                        if (i % 2 == 1) {
-                            setRest(false)
+                    else {
+                        for (let i = 1; i <= intervals; i++) {
+                            timeline.push(roundLength * i + readyLength)
                         }
-                        // rest
+                    }
+                    console.log("timeline: ", timeline)
+                    // loop through timeline to determine where current time stands
+                    for (let i = 0; i < timeline.length; i++) {
+                        // if there is rest time
+                        if (restLength > 0) {
+                            // round
+                            if (i % 2 == 0 && (timeline[i] <= currentTime && currentTime < timeline[i + 1])) {
+                                setRounds((Math.floor(i / 2)) + 1);
+                                setRest(false);
+                                setDuration(timeline[i + 1] - currentTime);
+                                console.log(i, ": round, ", duration, ": duration for round in condition 1")
+                            }
+                            // rest
+                            else if (i % 2 == 1 && (timeline[i] <= currentTime && currentTime < timeline[i + 1])) {
+                                setRounds(Math.floor(i / 2) + 1);
+                                setRest(true);
+                                setDuration(timeline[i + 1] - currentTime);
+                                console.log(i, ": round, ", duration, ": duration for rest in condition 2")
+                            }
+                        }
+                        // if there isn't rest time
                         else {
-                            setRest(true)
+                            if (timeline[i] <= currentTime && currentTime < timeline[i + 1]) {
+                                setRounds(i + 1);
+                                setDuration(timeline[i + 1] - currentTime);
+                                console.log(i, ": round, ", duration, ": duration for round in condition 3")
+
+                            }
                         }
                     }
                 }
             }
-            //////////////////////////////////////////////// TIME REMAINING ///////////////////////////////////////////////////////////
-            /////////////////////////////////////////////////// NO TIME REMAINING /////////////////////////////////////////////////////
+            // if complete
             else {
                 clearInterval(training);
                 setTraining(null);
-                setComplete(true) // reset the duration state variable
+                setComplete(true)
             }
-            /////////////////////////////////////////////////// NO TIME REMAINING /////////////////////////////////////////////////////
         }
-        setStopTime(null); // reset the stopTime state variable
-        if (ready && duration == -1 && rounds == 1) {
-            setReady(false)
-        }
-        if (duration == -1 && rounds != intervals) {
-            if (restLength > 0 && ready == false) {
-                setRest(!rest);
+        else {
+            if (ready && duration == -1 && rounds == 1) {
+                setReady(false)
             }
-            setRounds(prevCount => {
-                if (ready) { return prevCount }
-                if (rest && restLength != 0) { return prevCount + 1; }
-                if (restLength === 0) { return prevCount + 1; }
-                else { return prevCount; }
-            });
+            if (duration == -1 && rounds != intervals) {
+                if (restLength > 0 && ready == false) {
+                    setRest(!rest);
+                }
+                console.log("setRounds trigger in 2nd part of duration useEffect")
+                setRounds(prevCount => {
+                    if (ready) { return prevCount }
+                    if (rest && restLength != 0) { return prevCount + 1; }
+                    if (restLength === 0) { return prevCount + 1; }
+                    else { return prevCount; }
+                });
+            }
+            if (duration === -1 && rounds === intervals && !ready) {
+                clearInterval(training);
+                setTraining(null);
+                setComplete(true);
+                setStartTime(null);
+            }
         }
-        if (duration === -1 && rounds === intervals && !ready) {
-            clearInterval(training);
-            setTraining(null);
-            setComplete(true);
-            setStartTime(null);
-        }
-
+        setStopTime(null);
     }, [duration]);
-
-    // useEffect(() => {
-    //     console.log("appState useEffect")
-    //     // if Date.now() has been stored, which occurs in the appstate listener when app is in background
-    //     if (stopTime !== null) {
-    //         console.log("appState useEffect stopTime not null")
-    //         //const offSet = stopTime - startTime; 
-    //         //const elapsedTime = Date.now() - stopTime;
-    //         //const currentTime = Math.floor((offSet + elapsedTime) / 1000);
-    //         const currentTime = Math.floor((Date.now() - startTime) / 1000);
-    //         const remainingTime = totalDuration - currentTime;
-    //         //////////////////////////////////////////////// TIME REMAINING ///////////////////////////////////////////////////////////
-    //         // if the timer is not done
-    //         if (remainingTime > 0) {
-    //             // if should be in ready state
-    //             if (currentTime < readyLength) {
-    //                 //setReady(true); should already be set this way
-    //                 //setRest(false); should already be set this way
-    //                 console.log(totalDuration, " totalDuration")
-    //                 console.log(remainingTime, " remainingTime")
-    //                 console.log(readyLength, " readyLength")
-    //                 console.log(currentTime)
-    //                 console.log(readyLength - currentTime, "ready length duration")
-    //                 setDuration(readyLength - currentTime);
-    //             }
-    //             // if should be in round or rest state
-    //             else {
-    //                 setReady(false);
-    //                 // do logic with remaining duration to determine which state the app needs to be in 
-    //                 const timeline = [];
-    //                 for (var i = 1; i <= intervals; i++) {
-    //                     let ti_1 = (roundLength * i + restLength * (i - 1) + readyLength)
-    //                     let ti_2 = (roundLength * i + restLength * i + readyLength)
-    //                     if (i != intervals) {
-    //                         timeline.push(ti_1, ti_2)
-    //                     }
-    //                     else {
-    //                         timeline.push(ti_1)
-    //                     }
-    //                 }
-    //                 console.log(timeline)
-    //                 // loop through timeline to determine where current time stands
-    //                 for (var i = 0; i < timeline.length; i++) {
-    //                     // round
-    //                     if (i % 2 == 1) {
-    //                         setRest(false)
-    //                     }
-    //                     // rest
-    //                     else {
-    //                         setRest(true)
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         //////////////////////////////////////////////// TIME REMAINING ///////////////////////////////////////////////////////////
-    //         /////////////////////////////////////////////////// NO TIME REMAINING /////////////////////////////////////////////////////
-    //         else {
-    //             clearInterval(training);
-    //             setTraining(null);
-    //             setComplete(true) // reset the duration state variable
-    //         }
-    //         /////////////////////////////////////////////////// NO TIME REMAINING /////////////////////////////////////////////////////
-    //     }
-    //     setStopTime(null); // reset the stopTime state variable
-    // }, [appState])
 
     const onPressHandle = () => {
         if (roundLength == 0) {
@@ -245,6 +186,16 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
                     return prevCount - 1;
                 });
             }, 1000));
+            if (readyLength > 0) {
+                setReady(true)
+            }
+            else {
+                setReady(false)
+            }
+            setDuration(readyLength > 0 ? readyLength - 1 : roundLength - 1);
+            setRounds(1);
+            setRest(false);
+            setComplete(false);
         }
     };
 
@@ -267,6 +218,8 @@ const FightClock = ({ intervals, restLength, roundLength, readyLength }) => {
                     clearInterval(training);
                     setTraining(null);
                     setStartTime(null);
+                    setAlteringState(false);
+                    setStopTime(null);
                 }}
             >
                 <View style={styles.fightClockModalContainer}>
